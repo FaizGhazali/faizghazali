b6b6d9beac73b4e983260c9d30180a041f4b831f
faizGhazali <ahmadfaiz21@outlook.com>
2023-12-01 17:03:27 +0800

    some changes

--- pages/testing/index.js
+++ pages/testing/index.js
@@ -1,7 +1,22 @@
 import Image from 'next/image';
+import {useEffect,useState} from 'react';
 
 
 export default function testingPage() {
+  const [imageSrc, setImageSrc] = useState('http://placekitten.com/200/300');
+  useEffect(() => {
+    const getRandomSize = () => {
+      const width = Math.floor(Math.random() * (450 - 150 + 1)) + 150; // Random width between 150 and 450
+      const height = Math.floor(Math.random() * (450 - 150 + 1)) + 150; // Random height between 150 and 450
+      return `http://placekitten.com/${width}/${height}`;
+    };
+    // Change the image source after 3 seconds (for demonstration purposes)
+    const timer = setTimeout(() => {
+      setImageSrc(getRandomSize()); // New image source
+    }, 3000);
+
+    return () => clearTimeout(timer); // Clean up the timer on component unmount
+  }, []); // Empty dependency array ensures this effect runs only once
   return (
     <>
       <div className="flex-1/2 bg-red-300 border border-solid border-white">
@@ -35,7 +50,7 @@ export default function testingPage() {
 
         <div className="flex-1 bg-green-800">Main Content
         <Image
-        src="http://placekitten.com/200/300" // Path to your image in the public directory
+        src={imageSrc} // Path to your image in the public directory
         alt="Description of your image"
         width={500} // Set the width of the image
         height={300} // Set the height of the image
