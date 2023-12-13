f16cea9258d45491333960bd80582cce025ae024
faizGhazali <ahmadfaiz21@outlook.com>
2023-12-13 14:34:35 +0800

    .

--- pages/testing/index.js
+++ pages/testing/index.js
@@ -1,13 +1,12 @@
-import Image from 'next/image';
-import {useEffect,useState} from 'react';
-
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function testingPage() {
-  const [imageSrc, setImageSrc] = useState('http://placekitten.com/200/300');
+  const [imageSrc, setImageSrc] = useState("http://placekitten.com/200/300");
   useEffect(() => {
     const getRandomSize = () => {
-      const width = Math.floor(Math.random() * (450 - 150 + 1)) + 150; // Random width between 150 and 450
-      const height = Math.floor(Math.random() * (450 - 150 + 1)) + 150; // Random height between 150 and 450
+      const width = Math.floor(Math.random() * (450 - 150 + 1)) + 150;
+      const height = Math.floor(Math.random() * (450 - 150 + 1)) + 150;
       return `http://placekitten.com/${width}/${height}`;
     };
     // Change the image source after 3 seconds (for demonstration purposes)
@@ -33,28 +32,51 @@ export default function testingPage() {
       </div>
 
       <div className="flex bg-red-300 border border-solid border-white overflow-hidden ">
-      {/* max-h-screen */}
+        {/* max-h-screen */}
         <div className="w-1/5 bg-blue-800 text-right h-96 overflow-y-auto ">
-          <div className="mr-5 border border-black mt-10 mb-10">Total Project</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Add Project</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Delete Project</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Random Image</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Random Video</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Ramdom Quote</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Left Sidebar</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Left Sidebar</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Left Sidebar</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Left Sidebar</div>
-          <div className="mr-5 border border-black mt-10 mb-10">Left Sidebar</div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Total Project
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Add Project
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Delete Project
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Random Image
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Random Video
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Ramdom Quote
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Left Sidebar
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Left Sidebar
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Left Sidebar
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Left Sidebar
+          </div>
+          <div className="mr-5 border border-black mt-10 mb-10">
+            Left Sidebar
+          </div>
         </div>
 
-        <div className="flex-1 bg-green-800">Main Content
-        <Image
-        src={imageSrc} // Path to your image in the public directory
-        alt="Description of your image"
-        width={500} // Set the width of the image
-        height={300} // Set the height of the image
-      />
+        <div className="flex-1 bg-green-800">
+          Main Content
+          <div
+            className="bg-no-repeat bg-center w-500 h-300 border border-solid border-white "
+            style={{ backgroundImage: `url(${imageSrc})`, height: "300px",width:'500px',margin:'10px' }}
+          >
+            {/* Any content you might want to add within the div */}
+          </div>
         </div>
       </div>
     </>
